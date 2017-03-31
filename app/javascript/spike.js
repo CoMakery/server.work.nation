c27a0950d4cfc988a65b75a1c1be0112ccd860cd
Harlan T Wood and Noah Thorp <dev+harlan+noah@comakery.com>
2017-03-30 17:43:08 -0700

    Claims and confirmers WIP

--- app/javascript/spike.js
+++ app/javascript/spike.js
@@ -1,54 +1,44 @@
 import { Connect, Credentials, SimpleSigner } from 'uport-connect'
 import * as $ from 'jquery'
 
+const web3 = Connect.getWeb3()
+
 const d = (arg) => console.log(JSON.stringify(arg, null, 4))
 
-const harlan = '0x57fab088be2f8bfd5d4cbf849c2568672e4f3db3'
-const appPrivateKey = '4894506ba6ed1a2d21cb11331620784ad1ff9adf1676dc2720de5435dcf76ac2'
+// const harlan = '0x57fab088be2f8bfd5d4cbf849c2568672e4f3db3'
+// const appPrivateKey = '4894506ba6ed1a2d21cb11331620784ad1ff9adf1676dc2720de5435dcf76ac2'
 // const alicePrivateKey = '49e8f08170d4a514beff75a30300290f6d79845a06abfeb6080e1b8327bcb172'
 // const bobPrivateKey   = '85c4480b9a138dc67b0ed4f062bf4c7206e43b17d43287557771f67fec96639e'
 const appName = 'Work.nation'
 const clientId = '0xe2fef711a5988fbe84b806d4817197f033dde050'
-const signer = SimpleSigner(appPrivateKey)
+// const signer = SimpleSigner(appPrivateKey)
 const connect1 = new Connect(appName, { clientId })
-
-// /////////////////////////////////////////////////////////////
-
-const credentials = new Credentials({
-  appName,
-  address: harlan,
-  signer
-  // signer: SimpleSigner(alicePrivateKey)
-})
-
-// Requesting information from your users
-// To request information from your user you create a Selective Disclosure Request JWT and present it to your user in the web browser.
-//   The most basic request to get a users public uport identity details:
-credentials.createRequest().then(token => {
-  return d({token})  // eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJ0eXBlIjoic2hhcmVSZXEiLCJpc3MiOiIweDU3ZmFiMDg4YmUyZjhiZmQ1ZDRjYmY4NDljMjU2ODY3MmU0ZjNkYjMiLCJpYXQiOjE0OTA0MjYwMjI1MzZ9.oHyhhKmKKss8j40qq3gZ3mjk11cDhg_F6UD90cTLk63SuMwzOHljU9bEL1YyvuPGo67myJ4wXEk7XsrJmn3ACA
-
-//   return credentials.receive(token) // ---> Error: Signature invalid for JWT <---
-
-// }).then(profile => {
-//   return d({profile})
-})
-
-// Creating an attestation
-credentials.attest({
-  sub: harlan,
-    // exp: <future timestamp>, // If your information is not permanent make sure to add an expires timestamp
-  claims: {skill: 'Ruby'}
-}).then(attestation => {
-  return d({attestation})  // eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJzdWIiOiIweDU3ZmFiMDg4YmUyZjhiZmQ1ZDRjYmY4NDljMjU2ODY3MmU0ZjNkYjMiLCJpc3MiOiIweDU3ZmFiMDg4YmUyZjhiZmQ1ZDRjYmY4NDljMjU2ODY3MmU0ZjNkYjMiLCJpYXQiOjE0OTA0MjYwMjI1NTd9.BVPPlKUbbLW7X5jt3GaBPK7B4fwYBo-qFXrZy-EifpdDtRSGxSuisZdraBrFoX30eRz-b-9RoR_wjaylbmf64w
-})
-
-// /////////////////////////////////////////////////////////////
 
 let state = {
   userUportId: '',
   txHash: '',
   sendToAddr: '',
   sendToVal: '',
+}
+
+const attest = () => {
+  const claimAbi = [{'constant': true, 'inputs': [], 'name': 'claim', 'outputs': [{'name': '', 'type': 'string'}], 'payable': false, 'type': 'function'}, {'constant': false, 'inputs': [{'name': '_target', 'type': 'address'}, {'name': '_claimId', 'type': 'string'}], 'name': 'attest', 'outputs': [], 'payable': false, 'type': 'function'}, {'anonymous': false, 'inputs': [{'indexed': true, 'name': '_from', 'type': 'address'}, {'indexed': true, 'name': '_target', 'type': 'address'}, {'indexed': false, 'name': '_claimId', 'type': 'string'}], 'name': 'ClaimMade', 'type': 'event'}]
+  const claimContract = web3.eth.contract(claimAbi)
+  const claim = claimContract.at('0xf9c7ce3e77f7cc56657d99b28b9c70248ae2b6d6')
+
+  const target = '0x375c21b796facc074939e601c6320147d7344542'
+  const claimId = '/ipfs/QmXr74i6cuBtFVD7idvRVs4dyZt7wy2Sqgp1FziphLTHCK'
+
+  d('creating attestation...')
+
+  claim.attest('0x375c21b796facc074939e601c6320147d7344542', text, function(error, txhash) {
+    if (error) {
+      console.log(error)
+      return
+    }
+    $('#uport-status-txhash').text(txhash)
+    $('#uport-status-txhash').attr('href', 'https://ropsten.io/tx/' + txhash)
+  })
 }
 
 const uportConnect = () => {
@@ -62,44 +52,6 @@ const uportConnect = () => {
     attest()
     return
   }).catch(console.error)
-}
-
-const attest = () => {
-  d('Requesting attestation...')
-
-  const connect2 = new Connect(appName, { clientId, signer })
-
-  // const credentials = new Credentials({
-  //   appName: 'Test App',
-  //   address: '0xdb67cea13ef97b104dc80a72def566da03f5e999',
-  //   signer: signer
-  // })
-
-  const params = {
-    sub: state.userUportId,
-    claim: {name: state.name},
-    // claim: {skill: 'Ruby'},
-    // exp: new Date().getTime() + 2592000000  // expires in 30 days
-  }
-
-  d(222, params)
-
-  connect2.attestCredentials(params)
-  .then(res => {
-    console.log(222, res)
-    return
-  }).catch(err => {
-    console.error(333, err)
-  })
-
-  // credentials.attest(params, (attestation) => {
-  //   d(attestation)
-  //   connect.showRequest(attestation)
-  // })
-
-  // if (uport.pushToken) {
-  //   window.alert('Your credentials were sent directly to your phone')
-  // }
 }
 
 $(() => {
--- contracts/spike.sol
+++ contracts/spike.sol
@@ -1,16 +1,91 @@
 pragma solidity ^0.4.9;
 
-contract Claim {
+contract Claims {
 
-    string public claim;
+//    {claimant(address) : claim(string)[] }
+    mapping (address => string[]) public claims;
 
-    event ClaimMade(address indexed _from, address indexed _target, string _claimId);
+//    {claim : confirmer(address)[]}
+    mapping (string => address[]) claimConfirmers;
 
-    function attest(address _target, string _claimId) {
-        claim = _claimId;
-        ClaimMade(msg.sender, _target, _claimId);
+//    {address : trusted(address)[] }
+    mapping (address => address[]) public trusted;
+
+//    function Claim() {
+//        claims = {};
+//        claimConfirmers = {}; 
+//    }
+
+    function whoami() returns (address) {
+        return msg.sender;
+    }
+
+    function claim(string _claimId) {
+//        if (claims[msg.sender].empty?) {
+//            claims[msg.sender] = [_claimId];
+//        } 
+        claims[msg.sender].push(_claimId);
+    }
+
+    function confirm(string _claimId, address claimant) {
+        // TODO reject confirm of self
+        claimConfirmers[_claimId].push(msg.sender);
+        trusted[msg.sender].push(claimant);
+    }
+
+    function getClaimConfirmers(string _claimId) returns (address[] confirmers) {
+        return claimConfirmers[_claimId];
+    }
+
+    function getTrustedClaims(address[] peeps, int8 depth) returns(address[]) {
+        return trusted[msg.sender];
+    //        Return ppl if depth == 0
+    //        //newPpl = trusted[msg.sender]
+    //        //walk addresss trusted; 
+    //        For person in ppl {
+    //              getTrustedClaims(depth - 1)
+    //        }
+    //        getClaims(address);
+    //        getClaimConfirmers(ipfs-claim)
     }
 }
+
+
+
+
+//contract Claim {
+//
+//    string public claim;
+//
+//    event ClaimMade(address indexed _from, address indexed _target, string _claimId);
+//
+//    function attest(address _target, string _claimId) {
+//        claim = _claimId;
+//        ClaimMade(msg.sender, _target, _claimId);
+//    }
+//}
+
+
+// alice claims "alice used ROR on project X" (Claim C)
+// bob confirms Claim C
+// now, alice's further confirmations are trusted by Bob
+
+
+//  P <- [[claim]] -> P
+//
+//  claim <- [[confirmation]] -> Q
+//
+//  Q <- [trusted to confirm] <- P
+
+// maybe:
+//  Q <- [NOT trusted to confirm] <- P
+
+// search for RoR in my network:
+// 1. search to N levels: people I've confirmed / they've confirmed in anything
+// 2. within all those results, find the RoR confirmed claims
+// 3. return results: for each person, confirmed wihin my trust network + confirmed outside of network 
+
+
 
 //    mapping (address => mapping (address => bool)) claimExistsTo;
 //    mapping (address => mapping (address => bool)) claimExistsFrom;
