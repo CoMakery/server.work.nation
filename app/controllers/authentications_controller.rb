258f85bc9cea9b03640fa16ba9aead1e7710aa04
Harlan T Wood and Noah Thorp <dev+harlan+noah@comakery.com>
2017-04-05 00:18:46 -0700

    login

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,4 +1,9 @@
 class AuthenticationsController < ApplicationController
   def new
   end
+
+  def create
+    session[:uport_user_id] = params[:uport_user_id]
+    redirect_to root_path
+  end
 end
