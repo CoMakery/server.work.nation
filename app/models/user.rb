43628cef89f7197f3829801507d5054999f9ba5b
Harlan T Wood <dev+harlan@comakery.com>
2017-04-07 02:12:52 -0700

    use uport_address as main user "id"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_many :skills
 
+  def self.find(input)
+    find_by_uport_address(input)
+  end
+
+  def to_param
+    uport_address
+  end
+
   def as_json options={}
     {
         name: name,
