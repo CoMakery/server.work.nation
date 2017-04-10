39be2496346b8d6589d24667ee3a048aa24277cb
Harlan T Wood <dev+harlan@comakery.com>
2017-04-09 18:50:19 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,11 @@ if Rails.env.development?
   100.times do
     create :user, :random_skills, :random_address
   end
+
+  harlan = User.create! name: 'Harlan T Wood', uport_address: '0x57fab088be2f8bfd5d4cbf849c2568672e4f3db3'
+  harlan.skills.create! name: 'Ruby', project_count: 17, confirmation_count: 10
+  harlan.skills.create! name: 'Ethereum', project_count: 4, confirmation_count: 7
+  harlan.skills.create! name: 'uPort', project_count: 1, confirmation_count: 1
 end
 
 
