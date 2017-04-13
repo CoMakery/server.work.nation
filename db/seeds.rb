b4c0cc6c66d7057267beb5771ad21946c87c5116
Harlan Wood and Noah Thorp <harlan+noah@comakery.com>
2017-04-13 10:44:20 -0700

    Add Noah to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,11 @@ if Rails.env.development?
   harlan.skills.create! name: 'Ethereum', project_count: 4
   harlan.skills.create! name: 'uPort', project_count: 1
 
+  noah = User.create! name: 'Noah Thorp', uport_address: '0x62652a96e3e0f2ddb3fa637ceaac032fca154bc1'
+  noah.skills.create! name: 'Ruby', project_count: 17
+  noah.skills.create! name: 'Agile Project Management', project_count: 12
+  noah.skills.create! name: 'uPort', project_count: 1
+
   (USERS * 10).times do
     create :confirmation, :random_skill, :random_confirmer
   end
