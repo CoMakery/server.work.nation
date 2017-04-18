b14908778a22b862e3159953ea588a943484cfe0
Harlan Wood and Noah Thorp <harlan+noah@comakery.com>
2017-04-18 16:40:05 -0700

    Trust graph spike with confirmer_id and skill_claimant_id

--- trust_graph.sql
+++ trust_graph.sql
@@ -1,28 +1,28 @@
-WITH RECURSIVE search_graph(confirmer_id, skill_claimaint_id, depth, path, cycle) AS (
+WITH RECURSIVE search_graph(confirmer_id, skill_claimant_id, depth, path, cycle) AS (
   SELECT
     g.confirmer_id,
-    g.skill_claimaint_id,
-    1,
-    ARRAY [g.confirmer_id, g.skill_claimaint_id],
-    FALSE
+    g.skill_claimant_id,
+    1 as depth,
+    ARRAY [g.confirmer_id, g.skill_claimant_id] as path,
+    FALSE as cycle
   FROM confirmations g
-  WHERE confirmer_id = 15
+  WHERE confirmer_id = 1
   UNION
   SELECT
     g.confirmer_id,
-    g.skill_claimaint_id,
+    g.skill_claimant_id,
     sg.depth + 1,
-    path || g.skill_claimaint_id,
-    g.id = ANY (path)
+    path || g.skill_claimant_id,
+    g.confirmer_id = ANY (path)
   FROM confirmations g, search_graph sg
-  WHERE g.confirmer_id = sg.skill_claimaint_id
-        AND NOT cycle
+  WHERE g.confirmer_id = sg.skill_claimant_id
+  AND NOT cycle
 )
 SELECT DISTINCT
   path,
+  skill_claimant_id,
   confirmer_id,
-  skill_claimaint_id,
   depth
 FROM search_graph
-WHERE depth <= 3
+WHERE depth <= 4
 ORDER BY PATH;
