2f83aea1eb2c85496ac0e8f406040dc2824545b0
Noah Thorp <noah@comakery.com>
2017-04-18 19:07:26 -0700

    Return users with the desired skills

--- trust_graph.sql
+++ trust_graph.sql
@@ -1,7 +1,8 @@
-WITH RECURSIVE trust_graph(confirmer_id, skill_claimant_id, depth, path, confirmations_in_graph) AS (
+WITH RECURSIVE trust_graph(confirmer_id, skill_claimant_id, skill_id, depth, path, confirmations_in_graph) AS (
   SELECT
     conf1.confirmer_id,
     conf1.skill_claimant_id,
+    conf1.skill_id,
     1                                                   AS depth,
     ARRAY [conf1.confirmer_id, conf1.skill_claimant_id] AS path,
     ARRAY [conf1.id] AS confirmations_in_graph
@@ -11,6 +12,7 @@ WITH RECURSIVE trust_graph(confirmer_id, skill_claimant_id, depth, path, confirm
   SELECT
     conf2.confirmer_id,
     conf2.skill_claimant_id,
+    conf2.skill_id,
     previous_results.depth + 1,
     previous_results.path || conf2.skill_claimant_id,
     previous_results.confirmations_in_graph || conf2.id
@@ -21,9 +23,15 @@ WITH RECURSIVE trust_graph(confirmer_id, skill_claimant_id, depth, path, confirm
         AND NOT conf2.id = ANY (previous_results.confirmations_in_graph)
         AND NOT (previous_results.path || conf2.skill_claimant_id) = previous_results.path
 )
-SELECT DISTINCT path,
-  skill_claimant_id,
-  confirmer_id,
-  depth
+SELECT DISTINCT skill_claimant_id
 FROM trust_graph
-ORDER BY PATH;
+WHERE skill_id = 1;
+
+-- SELECT DISTINCT path,
+--   skill_claimant_id,
+--   confirmer_id,
+--   skill_id,
+--   depth
+-- FROM trust_graph
+--   WHERE skill_id = 5
+-- ORDER BY PATH;
