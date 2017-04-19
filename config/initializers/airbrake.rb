0dbe932ea363f5b8c0db641ff45616ade6646001
Harlan T Wood <dev+harlan@comakery.com>
2017-04-18 22:01:48 -0700

    Tweak Airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -7,7 +7,7 @@ if ENV['AIRBRAKE_PROJECT_ID'].present? && ENV['AIRBRAKE_API_KEY'].present?
     c.project_key = ENV['AIRBRAKE_API_KEY']
     c.root_directory = Rails.root
     c.logger = Rails.logger
-    c.environment = Rails.env
+    c.environment = ENV['APP_NAME'] || Rails.env
     c.ignore_environments = %w[development test]
 
     # A list of parameters that should be filtered out of what is sent to
@@ -17,7 +17,11 @@ if ENV['AIRBRAKE_PROJECT_ID'].present? && ENV['AIRBRAKE_API_KEY'].present?
     # Alternatively, you can integrate with Rails' filter_parameters.
     # Read more: https://goo.gl/gqQ1xS
     # c.blacklist_keys = Rails.application.config.filter_parameters
-    c.blacklist_keys = [/password/i, /authorization/i]
+    c.blacklist_keys = [
+      /password/i,
+      /authorization/i,
+      /api_?key/i,
+    ]
   end
 
 end
