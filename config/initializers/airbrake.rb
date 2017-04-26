214aa1e02860ecfadd20f71ef20cc6b6b8f8731b
Harlan T Wood <dev+harlan@comakery.com>
2017-04-25 22:36:40 -0700

    Only load Airbrake if env vars present

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,27 +1,23 @@
-if ENV['AIRBRAKE_PROJECT_ID'].present? && ENV['AIRBRAKE_API_KEY'].present?
+require 'airbrake/sidekiq/error_handler'
 
-  require 'airbrake/sidekiq/error_handler'
+Airbrake.configure do |c|
+  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_key = ENV['AIRBRAKE_API_KEY']
+  c.root_directory = Rails.root
+  c.logger = Rails.logger
+  c.environment = ENV['APP_NAME'] || Rails.env
+  c.ignore_environments = %w[development test]
 
-  Airbrake.configure do |c|
-    c.project_id = ENV['AIRBRAKE_PROJECT_ID']
-    c.project_key = ENV['AIRBRAKE_API_KEY']
-    c.root_directory = Rails.root
-    c.logger = Rails.logger
-    c.environment = ENV['APP_NAME'] || Rails.env
-    c.ignore_environments = %w[development test]
-
-    # A list of parameters that should be filtered out of what is sent to
-    # Airbrake. By default, all "password" attributes will have their contents
-    # replaced.
-    # https://github.com/airbrake/airbrake-ruby#blacklist_keys
-    # Alternatively, you can integrate with Rails' filter_parameters.
-    # Read more: https://goo.gl/gqQ1xS
-    # c.blacklist_keys = Rails.application.config.filter_parameters
-    c.blacklist_keys = [
-      /password/i,
-      /authorization/i,
-      /api_?key/i,
-    ]
-  end
-
+  # A list of parameters that should be filtered out of what is sent to
+  # Airbrake. By default, all "password" attributes will have their contents
+  # replaced.
+  # https://github.com/airbrake/airbrake-ruby#blacklist_keys
+  # Alternatively, you can integrate with Rails' filter_parameters.
+  # Read more: https://goo.gl/gqQ1xS
+  # c.blacklist_keys = Rails.application.config.filter_parameters
+  c.blacklist_keys = [
+    /password/i,
+    /authorization/i,
+    /api_?key/i,
+  ]
 end
