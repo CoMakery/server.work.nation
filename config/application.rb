a5984914daeeb7c291c905dbd6576d573ed512d5
Harlan T Wood <dev+harlan@comakery.com>
2017-05-02 11:09:06 -0700

    log to stdout in dev server

--- config/application.rb
+++ config/application.rb
@@ -38,12 +38,12 @@ module WorkNation
 
     config.eager_load = false
 
-    # if ENV['RAILS_LOG_TO_STDOUT'].present?
-    #   logger           = ActiveSupport::Logger.new(STDOUT)
-    #   logger.formatter = config.log_formatter
-    #   config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    # end
-    #
+    if ENV['RAILS_LOG_TO_STDOUT'].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
+
     # # Use a different logger for distributed setups.
     # # require 'syslog/logger'
     # # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
