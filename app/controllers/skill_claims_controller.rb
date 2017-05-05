36e6d6efa9ab943610678045c5f060d9317c6942
Harlan T Wood <dev+harlan@comakery.com>
2017-05-05 02:32:22 -0700

    Confirmable claims do not include your own claims

--- app/controllers/skill_claims_controller.rb
+++ app/controllers/skill_claims_controller.rb
@@ -2,7 +2,11 @@ class SkillClaimsController < ApplicationController
   # A list of skill claims, marked as confirmed or not by the specified user
   # GET /users/0x5d2e5e76624b05377dd9405df253e877532f9733/confirmables
   def confirmables
-    skill_claims = SkillClaim.includes(:user).order('created_at DESC').limit(300)
+    skill_claims = SkillClaim
+                   .where.not('users.uport_address': params[:user_uport_address])
+                   .includes(:user)
+                   .order('skill_claims.created_at DESC')
+                   .limit(300)
     render json: skill_claims.as_json(
       user: true,
       project: true,
