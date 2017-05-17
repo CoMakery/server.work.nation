aef870b8844a066bd3fc3eb5d5574981ceb4ffdf
Harlan T Wood <dev+harlan@comakery.com>
2017-05-17 14:29:51 -0700

    Stop crash if user not found; remove `@` symbols

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,22 @@ class UsersController < ApplicationController
   def index
     if params[:perspective]
       trust_graph_root_user = User.find_by(uport_address: params[:perspective])
-      @users = trust_graph_root_user.search_trust_graph(params[:skill], depth: params[:depth])
+      users = if trust_graph_root_user.nil?
+        []
+      else
+        trust_graph_root_user.search_trust_graph(params[:skill], depth: params[:depth])
+      end
     else
-      @users = User.limit(200).includes(:skill_claims)
+      users = User.limit(200).includes(:skill_claims)
     end
 
-    render json: @users.as_json(skills: true)
+    render json: users.as_json(skills: true)
   end
 
   # GET /users/0x01d3b5eaa2e305a1553f0e2612353c94e597449e (uPort address)
   def show
-    @user = User.find_or_create_by!(uport_address: params[:uport_address])
-    render json: @user.as_json(projects: true, skills: true, confirmations: true)
-    UpdateProfile.perform_async @user.to_param # update profile info from uPort async
+    user = User.find_or_create_by!(uport_address: params[:uport_address])
+    render json: user.as_json(projects: true, skills: true, confirmations: true)
+    UpdateProfile.perform_async user.to_param # update profile info from uPort async
   end
 end
